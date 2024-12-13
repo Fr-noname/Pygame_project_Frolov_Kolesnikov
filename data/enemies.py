aec9f9c2d0c8395fd8eacedaa0229799e1798274
Fr-noname <frnikita.91@gmail.com>
2024-12-13 21:25:31 +0300

    Классы врагов + функции для их инита

--- data/enemies.py
+++ data/enemies.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 
 class Enemy:
-    def __init__(self, id, position, k_in, k_out, r):  # k_in - то, сколько % урона получает моб, r - радиус атаки.
+    def __init__(self, id, position, k_in, k_out, r, dam):  # k_in - то, сколько % урона получает моб, r - радиус атаки.
         self.id = id
         self.pos = position
         self.hp = 100
@@ -11,6 +11,7 @@ class Enemy:
         self.k_in = k_in
         self.k_out = k_out  # k_out - то, сколько % урона получает игрок от моба
         self.radius = r
+        self.damage = dam
 
     def atack(self, player_pos):
         if sqrt(int(player_pos[0]) ** 2 + int(player_pos[1]) ** 2) <= self.radius:
@@ -28,8 +29,22 @@ class Enemy:
 
 class Archer(Enemy):
     def damaging(self):
-        a = random.randrange(0, 2)
-        if a != 0:
+        a = random.randrange(0, 4)
+        dam = random.randrange(self.damage)
+        if a == 0:  # Попал
             pass
-        else:
+        else:  # не попал
             pass
+
+
+class Tank(Enemy):
+    def damaging(self):
+        a = random.randrange(10, self.damage)
+        return a * self.k_out
+
+
+class SwordMan(Enemy):
+    def damaging(self):
+        a = random.randrange(10, self.damage)
+        return a
+
