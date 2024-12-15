c3e4053f2d12f3d46f8235e9a35282c7f19e69b8
Fr-noname <frnikita.91@gmail.com>
2024-12-15 09:54:10 +0300

    Персонажи

---
+++ data/weapons.py
@@ -0,0 +1,31 @@
+from math import sqrt
+from time import sleep
+
+
+class Swords:
+    def __init__(self, damage, attack_speed, r, s):  # r - дальность атаки, s - толщина
+        self.damaga = damage
+        self.attack_speed = attack_speed
+        self.r = r
+        self.s = s
+        self.min_s = 9999999999999999999999999999
+
+    def atack(self, spisok_mob_id, player_pos):
+        self.min_s = 9999999999999999999999999999
+        min_id = 0
+        for i in spisok_mob_id:
+            id = i[0]
+            x, y = i[1]
+            s_to_mob = sqrt((int(player_pos[0]) - int(x)) ** 2 +
+                            (int(player_pos[1]) - int(y) ** 2))
+            if s_to_mob <= self.min_s:
+                self.min_s = s_to_mob
+                min_id = id
+        if self.min_s <= self.r:
+            self.deal_damage(min_id)
+
+    def deal_damage(self, id):
+        pass
+
+    def kd(self):
+        sleep(1 / self.attack_speed)
