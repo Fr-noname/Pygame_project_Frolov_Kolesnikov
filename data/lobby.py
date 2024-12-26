308c5e68fee516d91b289c35b77f0206a1811dc5
Fr-noname <frnikita.91@gmail.com>
2024-12-26 15:28:42 +0300

    Исправил разрешение

--- data/lobby.py
+++ data/lobby.py
@@ -1,11 +1,11 @@
-import os
-import sys
 
+from data import functions
 import pygame
 
 
 def lobby():
     pygame.init()
+    image = functions.load_image('lobby.png')
     size = w, h = (1920, 1080)
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
@@ -14,11 +14,15 @@ def lobby():
     dt = 0
 
     player_pos = pygame.Vector2(w // 2 + 1, h // 2 + 1)
+    screen.blit(image, (1, 1))
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+
         screen.fill("Black")
     pygame.quit()
 
